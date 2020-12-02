b555ae8d5e7114636d7b5d4a4f27d2a0f583a72c
bcpratt2000 <bcpratt2000@gmail.com>
2020-12-01 22:19:31 -0700

    added basic very compressing functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+pub mod compress;
 fn main() {
-    println!("Hello, world!");
+	let mut s = String::from("Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hell");
+    let compressed_vector: Vec<u32> = compress::compress(64, &mut s);
+    for i in compressed_vector.iter(){
+		println!("{}", i);
+	}
 }
