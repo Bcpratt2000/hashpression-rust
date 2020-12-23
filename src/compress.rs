e0eea27e9b239d202460b0c5796b76ffa7d42c25
Bcpratt2000 <bcpratt2000@gmail.com>
2020-12-23 01:20:33 -0700

    working on file Writing

--- src/compress.rs
+++ src/compress.rs
@@ -1,26 +1,30 @@
 use hash32::FnvHasher;
 use hash32::Hasher;
 
-pub fn compress(block_size: usize, s: &mut String) -> Vec<u32>{
-	//pad end with spaces to be divisible by block_size
-	for _ in 0..block_size-(s.len()%block_size){
-		s.push(' ');
-	}
+pub fn compress(block_size: usize, s: &mut String) -> Vec<u32> {
+    //pad end with spaces to be divisible by block_size
+    if s.len() % block_size != 0 {
+        for _ in 0..block_size-(s.len() % block_size) {
+            s.push(' ');
+        }
+    }
 
-	//declare vairables
-	let mut to_ret: Vec<u32> = Vec::new();
-	let mut hasher;
+    //declare vairables
+    let mut to_ret: Vec<u32> = Vec::new();
+    let mut hasher;
 
-	for i in 1..s.len()/block_size{
-		//initalize hasher every loop because hasher.finish() does not clear it
-		hasher = FnvHasher::default();
+    for i in 0..(s.len() / block_size) {
+        //initalize hasher every loop because hasher.finish() does not clear it
+        hasher = FnvHasher::default();
 
-		//write block to hash to hasher
-		hasher.write(s.get(block_size*i..block_size*i+block_size)
-							.unwrap()
-							.as_bytes());
-		//write hash as a new entry in the vector
-		to_ret.push(hasher.finish());
-	}
-	to_ret
+        //write block to hash to hasher
+        hasher.write(
+            s.get(block_size * i..block_size * i + block_size)
+                .unwrap()
+                .as_bytes(),
+        );
+        //write hash as a new entry in the vector
+        to_ret.push(hasher.finish());
+    }
+    to_ret
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
 pub mod compress;
+
+use bincode;
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-	let mut s = String::from("Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hell");
+    let mut s = String::from("sdf");
     let compressed_vector: Vec<u32> = compress::compress(64, &mut s);
-    for i in compressed_vector.iter(){
-		println!("{}", i);
-	}
+
+    let encoded: Vec<u8> = bincode::serialize(&compressed_vector).unwrap();
+
+    {
+        let mut file = File::create("testFile").unwrap();
+        file.write_all(&encoded);
+        file.sync_all().unwrap();
+    }
+
+    for i in compressed_vector.iter() {
+        println!("{:b}", i);
+    }
 }
