caf239524e70b41bec9fbe31b8b817af052b2e52
bcpratt2000 <bcpratt2000@gmail.com>
2021-01-05 16:27:52 -0700

    implimented hashmap decoding

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
-pub mod compress;
-pub mod decompress;
-pub mod file_io;
+mod compress;
+mod decompress;
+mod file_io;
 
 use std::time::Instant;
 
 fn main() {
     const FILE_NAME: &str = "testFile.hps";
-    const BLOCK_SIZE: usize = 1;
-    // {
-    //     let mut to_compress: Vec<u8> = file_io::read_from_file("videoFile.mp4");
-    //     // let mut to_compress: Vec<u8> = String::from("Hello world!Hello world!Hello world!Hello world!Hello world!Hello worl d!").into_bytes();
+    const BLOCK_SIZE: usize = 5;
+    {
+        let mut to_compress: Vec<u8> = file_io::read_from_file("EnglishShortened.txt");
+        // let mut to_compress: Vec<u8> = String::from("Hello world!Hello world!Hello world!Hello world!Hello world!Hello worl d!").into_bytes();
 
-    //     let compressed: Vec<u32> = compress::compress(BLOCK_SIZE, &mut to_compress);
+        let compressed: Vec<u32> = compress::compress(BLOCK_SIZE, &mut to_compress);
 
-    //     file_io::write_to_file_serialized(&compressed, FILE_NAME);
-    // }
+        file_io::write_to_file_serialized(&compressed, FILE_NAME);
+    }
 
     let mut from_file = file_io::read_from_file_deserialized(FILE_NAME);
 
@@ -22,7 +22,7 @@ fn main() {
     let decompressed = decompress::decompress(&mut from_file, BLOCK_SIZE);
     println!("Seconds to decompress: {}\n", start.elapsed().as_millis() as f64/1000 as f64);
 
-    file_io::write_vec8_to_file(&decompressed, "testFile.mp4");
+    // file_io::write_vec8_to_file(&decompressed, "testFile.mp4");
 
     // println!("{}", String::from_utf8_lossy(&decompressed));
 }
